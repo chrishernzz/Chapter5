476d2db63298581a8534d4a720d68fdfaca4ef50
Christian Hernandez <christianhernandez1014@gmail.com>
2023-09-30 18:00:06 -0700

    Add project files.

---
+++ Chapter5/Vector.h
@@ -0,0 +1,62 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<string>
+#include<string>
+#include<fstream>
+#include<algorithm>
+#include"Student.h"
+using namespace std;
+
+//input (1) header files inputs validation
+char inputChar(string prompt, string lookup);
+int inputInteger(string prompt, int startRange, int endRange);
+double inputDouble(string prompt, double startRange, double endRange);
+
+class Vector{
+private:
+	//calling the student class
+	vector<student> data;
+public: 
+	//defualt constructor that will have the student class default constructor
+	Vector();
+
+	//member function to clear data
+	void clearData();
+	//member function to resize
+	void resizeData();
+	//member function to get front element
+	void reserveData();
+	//member function to read file
+	void readFromFile(string& fileName);
+	//member function to close file
+	void closeFile(string& filename);
+	//member function to pop back
+	void popBackElement();
+	//member function to get front element
+	void printFrontElement();
+	//member function to get back element
+	void printBackElement();
+	//member function to get the index element
+	void lookForIndex();
+	//member function to get the memory address of the first and last iterator, do it in reverse as well  
+	void beginIterator();
+	void endIterator();
+	void beginEndIterator();
+	void reverseBeginIterator();
+	void reverseEndIterator();
+	void reverseBeginAndEndIterator();
+	//member function to erase a single element, after the first element
+	void eraseIterator();
+	//member function to erase the whole element using it
+	void eraseRangeIterator();
+	//member function to insert a new name, grade, and gpa after the first element
+	void insertIterator();
+	//member function to get the first data and swap to the other data (new vector) and goin to sort them
+	void swapVector();
+	void sortVector();
+
+	//member function to get the size
+	//member function
+	void mainMenu();
+};
---
+++ Chapter5/main.cpp
@@ -0,0 +1,66 @@
+//Christian Hernandez
+//Date: 10/4/2023
+//subscription: CMPR131
+
+#include<iostream>
+#include"input (1).h"
+#include"Vector.h"
+using namespace std;
+
+//prototype
+int menuOption();
+void challenge1();
+//void challenge2();
+//void challenge3();
+
+int main()
+{
+	do {
+		switch (menuOption()) {
+		case 0: exit(1); break;
+		case 1: challenge1(); break;
+		//case 2: challenge2(); break;
+		//case 3: challenge3(); break;
+		default: cout << "\t\tERROR - Invalid option. Please re-enter."; break;
+		}
+		cout << "\n";
+		system("pause");
+	} while (true);
+
+	return EXIT_SUCCESS;
+}
+
+//precondition: making a menu option
+//postcondition: it will return an int (the choice of the menu)
+int menuOption() {
+	system("cls");
+	cout << "\n\tCMPR131 Chapter 5: Vector and List Container by Christian Hernandez(9/28/2023)";
+	cout << "\n\t" << string(100, char(205));
+	cout << "\n\t\t1> Vector container";
+	cout << "\n\t\t2> List container";
+	cout << "\n\t\t3> Application using Vector and/or List container";
+	cout << "\n\t" << string(100, char(205));
+	cout << "\n\t\t0> Exit";
+	cout << "\n\t" << string(100, char(205));
+	//calling the input.h and we are inputting c1 from 0 through 3
+	return inputInteger("\n\t\tOption: ", 0, 3);
+}
+
+//precondition: going to call my Complex class that has the real number and imaginary number
+//postcondition: going to then call my displayInformation function that will have all the information and the menus as well
+void challenge1() {
+	Vector menu;
+	menu.mainMenu();
+}
+//precondition: going to call my Rational Class
+//postcondition: going to then displayInformation function so that will give me the information for rational
+void challenge2() {
+	//Rational menu;
+	//menu.RationalMenu();
+}
+//precondition: going to call my Polynomial class
+//postcondition: going to then call my displayInformation function that will have all the information and the menus as well
+void challenge3() {
+	//Polynomial menu;
+	//menu.main();
+}
